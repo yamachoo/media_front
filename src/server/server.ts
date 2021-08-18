0c5a783d692d9ac63e462175535bea429cc25444
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-08-18 22:54:02 +0900

    add axios server

---
+++ src/server/server.ts
@@ -0,0 +1,16 @@
+import axios from 'axios'
+import {
+  DEVELOPMENT_SERVER_URL,
+  ENDPOINT_PATH,
+  ENDPOINT_VERSION
+} from '../constants'
+
+const ENDPOINT_URL =
+  process.env.NODE_ENV === 'development'
+    ? `${DEVELOPMENT_SERVER_URL}/${ENDPOINT_PATH}/${ENDPOINT_VERSION}`
+    : `${window.location.origin}/${ENDPOINT_PATH}/${ENDPOINT_VERSION}`
+
+export const server = axios.create({
+  baseURL: ENDPOINT_URL,
+  timeout: 5000
+})
