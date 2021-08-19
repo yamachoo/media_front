940588578a08ab8037f1f15c137c538588d22918
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-08-19 22:57:58 +0900

    fix DEVELOPMENT_SERVER_URL

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
-export const DEVELOPMENT_SERVER_URL = 'http://localhost:3000'
+export const DEVELOPMENT_SERVER_URL = 'http://localhost:8080'
 export const ENDPOINT_PATH = 'api'
 export const ENDPOINT_VERSION = 'v1'
