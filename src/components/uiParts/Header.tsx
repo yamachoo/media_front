9da14d00edcb4611746948fb433d9ca5e4e47fce
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-08-20 22:27:43 +0900

    add uiParts/Header

---
+++ src/components/uiParts/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header: React.FC = () => {
+  return (
+    <header className="navbar bg-neutral">
+      <div className="mx-4">
+        <span className="text-lg font-bold">Media</span>
+      </div>
+    </header>
+  )
+}
