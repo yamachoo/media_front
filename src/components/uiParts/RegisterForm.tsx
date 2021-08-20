7cde8fa0b132d878a84c153d9b0e06787f5205d5
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-08-20 23:08:25 +0900

    add RegisterForm

---
+++ src/components/uiParts/RegisterForm.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { userRegister } from '../../server/api/auth'
+import type { RegisterQuery } from '../../types/global'
+
+export const RegisterForm: React.FC = () => {
+  const { register, handleSubmit } = useForm<RegisterQuery>()
+  const onSubmit = async (data: RegisterQuery) => {
+    try {
+      await userRegister(data)
+      console.log('success')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <div className="card bg-base-200 p-10 max-w-md mx-auto">
+      <form onSubmit={handleSubmit(onSubmit)} className="form-control">
+        <input
+          {...register('name', {
+            required: true,
+            maxLength: 24,
+            minLength: 1
+          })}
+          placeholder="名前"
+          className="input mb-6"
+        />
+        <input
+          {...register('email', { required: true, maxLength: 100 })}
+          placeholder="メールアドレス"
+          className="input mb-6"
+        />
+        <input
+          {...register('password', {
+            required: true,
+            maxLength: 24,
+            minLength: 8
+          })}
+          placeholder="パスワード"
+          className="input mb-6"
+        />
+        <button type="submit" className="btn btn-primary">
+          新規登録
+        </button>
+      </form>
+    </div>
+  )
+}
