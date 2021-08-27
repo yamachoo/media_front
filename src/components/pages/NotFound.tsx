33aba88cab26eef66ee83d1add825d874f4f5827
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-08-27 21:47:40 +0900

    add NotFound

---
+++ src/components/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NotFound: React.FC = () => {
+  return (
+    <div className="h-screen flex items-center justify-center p-2">
+      404 Not Found
+    </div>
+  )
+}
