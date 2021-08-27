acb8a9a38beb099527b159accd935aee75d81d06
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-08-27 21:47:18 +0900

    add Register

---
+++ src/components/pages/Register.tsx
@@ -0,0 +1,9 @@
+import { RegisterForm } from '../uiParts/RegisterForm'
+
+export const Register: React.FC = () => {
+  return (
+    <div className="h-screen flex items-center justify-center p-2">
+      <RegisterForm />
+    </div>
+  )
+}
