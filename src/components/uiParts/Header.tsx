8ef594533cea543a09c6adaa0ed7ed46ed136503
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-08-28 22:47:02 +0900

    update Header

--- src/components/uiParts/Header.tsx
+++ src/components/uiParts/Header.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import { useContext } from 'react'
+import { UserContext } from '../App'
+import { LoginButton } from './LoginButton'
+import { LogoutButton } from './LogoutButton'
 
 export const Header: React.FC = () => {
+  const { user } = useContext(UserContext)
+
   return (
-    <header className="navbar bg-neutral">
-      <div className="mx-4">
-        <span className="text-lg font-bold">Media</span>
+    <header className="navbar bg-neutral absolute w-screen">
+      <div className="flex-1 px-2 mx-2">
+        <span className="text-lg font-bold text-white">Media</span>
+      </div>
+      <div className="flex-none">
+        {Object.keys(user).length !== 0 ? <LogoutButton /> : <LoginButton />}
       </div>
     </header>
   )
