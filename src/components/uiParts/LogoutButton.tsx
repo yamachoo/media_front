8e5ca412d9454944ea3ca6494ea18cb846a0b416
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-08-28 22:46:41 +0900

    update LogoutButton

--- src/components/uiParts/LogoutButton.tsx
+++ src/components/uiParts/LogoutButton.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import { useContext } from 'react'
+import { useHistory } from 'react-router-dom'
 import { userLogout } from '../../server/api/auth'
+import type { User } from '../../types/global'
+import { UserContext } from '../App'
 
 export const LogoutButton: React.FC = () => {
+  const { setUser } = useContext(UserContext)
+  const history = useHistory()
   const logout = async () => {
     try {
       await userLogout()
-      console.log('success')
+      setUser({} as User)
+      history.push('/')
     } catch (error) {
       console.log(error)
     }
