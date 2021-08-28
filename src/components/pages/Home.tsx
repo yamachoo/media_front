c1b17a4cfd48794bac3a68b7dc6ad0c37b13e71f
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-08-28 22:44:09 +0900

    add Home

---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,9 @@
+export const Home: React.FC = () => {
+  return (
+    <div className="pt-20">
+      <div className="container mx-auto px-4">
+        <p className="text-center">home</p>
+      </div>
+    </div>
+  )
+}
