f360f1ab85f732934d350a8409b336e2e05a43a2
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-08-30 23:02:10 +0900

    add Homeにメディア一覧を追加

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from 'react'
+import { SERVER_URL } from '../../constants'
+import { getPictures } from '../../server/api/pictures'
+import type { Picture } from '../../types/global'
+import { PictureCard } from '../uiParts/PictureCard'
+
 export const Home: React.FC = () => {
+  const [pictures, setPictures] = useState([] as Picture[])
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const result = await getPictures()
+        setPictures(result)
+      } catch (error) {
+        console.error(error)
+      }
+    })()
+  }, [])
+
   return (
     <div className="pt-20">
       <div className="container mx-auto px-4">
-        <p className="text-center">home</p>
+        {pictures.map((picture, i) => (
+          <div key={i} className="mb-4">
+            <PictureCard
+              src={SERVER_URL.concat('/static/', picture.path)}
+              name={picture.filename}
+            />
+          </div>
+        ))}
       </div>
     </div>
   )
