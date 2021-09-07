d69d0ea0fab59597edfaada3d544c1f25a978519
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-09-07 22:47:53 +0900

    fix Home

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -22,9 +22,9 @@ export const Home: React.FC = () => {
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
           {pictures.map((picture, i) => (
-            <div key={i} className="mb-4">
+            <div key={i} className="mb-2">
               <PictureCard
-                src={SERVER_URL.concat('/static/', picture.path)}
+                src={SERVER_URL.concat(picture.path)}
                 name={picture.filename}
               />
             </div>
